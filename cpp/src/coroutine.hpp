e797cd667884afea5d0689e68e4f6fcb3f9700a7
AnirudhRahul <anirudhrahul@anjurahul.com>
2021-02-08 01:28:04 -0500

    test

--- cpp/src/coroutine.hpp
+++ cpp/src/coroutine.hpp
@@ -1,9 +1,7 @@
 # if __has_include(<experimental/coroutine>)
 #       include <experimental/coroutine>
 namespace std {
-    namespace suspend_always = experimental::suspend_always ;
-    namespace suspend_never = experimental::suspend_never ;
-    namespace coroutine_handle = experimental::coroutine_handle ;
+    using namespace experimental;
 }
 # else
 #   include <coroutine>
