1bdc0eba59c84f04560991d7de919fc28418b815
AnirudhRahul <anirudhrahul@anjurahul.com>
2021-02-08 01:26:05 -0500

    namespace hoisting

--- cpp/src/coroutine.hpp
+++ cpp/src/coroutine.hpp
@@ -1,7 +1,9 @@
 # if __has_include(<experimental/coroutine>)
 #       include <experimental/coroutine>
 namespace std {
-    namespace coroutine = experimental::coroutine;
+    namespace suspend_always = experimental::suspend_always ;
+    namespace suspend_never = experimental::suspend_never ;
+    namespace coroutine_handle = experimental::coroutine_handle ;
 }
 # else
 #   include <coroutine>
